dda064af87a4fc0270e0144a502fb2e47f8f1e89
denis <kagane@yandex.ru>
2023-12-10 19:11:03 +0300

    LinkedList

--- src/main/java/collection/LinkedListExample.java
+++ src/main/java/collection/LinkedListExample.java
@@ -6,6 +6,8 @@ import java.util.LinkedList;
 
 public class LinkedListExample {
     public static void main(String[] args) {
+        final String LINKED_LIST_EQUALS = "LinkedList = ";
+
         Student2 student1 = new Student2("Иван", 3);
         Student2 student2 = new Student2("Николай", 2);
         Student2 student3 = new Student2("Елена", 1);
@@ -19,17 +21,17 @@ public class LinkedListExample {
         linkedList.add(student4);
         linkedList.add(student5);
 
-        System.out.println("LinkedList = " + linkedList);
+        System.out.println(LINKED_LIST_EQUALS + linkedList);
         System.out.println(linkedList.get(2));
 
         Student2 student6 = new Student2("Денис", 3);
         Student2 student7 = new Student2("Игорь", 4);
         linkedList.add(student6);
-        System.out.println("LinkedList = " + linkedList);
+        System.out.println(LINKED_LIST_EQUALS + linkedList);
         linkedList.add(1, student7);
-        System.out.println("LinkedList = " + linkedList);
+        System.out.println(LINKED_LIST_EQUALS + linkedList);
         linkedList.remove(3);
-        System.out.println("LinkedList = " + linkedList);
+        System.out.println(LINKED_LIST_EQUALS + linkedList);
     }
 }
 
