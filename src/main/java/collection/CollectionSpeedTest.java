36f878979074dddfcc1b0803941f5e6d51fa6fb6
denis <kagane@yandex.ru>
2023-12-11 13:25:24 +0300

    CollectionSpeedTest

--- src/main/java/collection/CollectionSpeedTest.java
+++ src/main/java/collection/CollectionSpeedTest.java
@@ -1,35 +1,34 @@
 package collection;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CollectionSpeedTest {
     public static void main(String[] args) throws IOException {
-        final String PATH = "src/main/resources/input.txt";
+        final String PATH_TO_INPUT_FILE = "src/main/resources/input.txt";
 
         long endTime;
         long startTime;
-        int index = 10;
+        int index = 10000000;
         int maxSize = 20000000;
 
-//        File file = new File(PATH);
-//        FileOutputStream fileOutputStream = new FileOutputStream(file);
-//
-//        StringBuilder sequenceBuilder = new StringBuilder();
-//        for (int i = 0; i <= maxSize; i++) {
-//            sequenceBuilder.append(i).append(",");
-//        }
-//        String sequence = sequenceBuilder.toString();
-//        fileOutputStream.write(sequence.getBytes());
-//        fileOutputStream.close();
+        File file = new File(PATH_TO_INPUT_FILE);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
 
-        String inputFileName = PATH;
+        StringBuilder sequenceBuilder = new StringBuilder();
+        for (int i = 0; i <= maxSize; i++) {
+            sequenceBuilder.append(i).append(",");
+        }
+        String sequence = sequenceBuilder.toString();
+        fileOutputStream.write(sequence.getBytes());
+        fileOutputStream.close();
+
         List<String> list = new ArrayList<>();
-        try (BufferedReader r = new BufferedReader(new FileReader(inputFileName))) {
+        try (BufferedReader r = new BufferedReader(new FileReader(PATH_TO_INPUT_FILE))) {
             String string = r.readLine();
             list = Arrays.asList(string.split(","));
         } catch (IOException e) {
@@ -42,8 +41,8 @@ public class CollectionSpeedTest {
             arrayList.add(Integer.valueOf(s));
             linkedList.add(Integer.valueOf(s));
         }
-        arrayList.set(0,111);
-        linkedList.set(0, 222);
+//        arrayList.set(0, 111);
+//        linkedList.set(0, 222);
 
         //ArrayList
         TestCollectionsSpeed.compareTimeOfRemove(index, arrayList, "ArrayList");
@@ -53,7 +52,7 @@ public class CollectionSpeedTest {
         //LinkedList
         TestCollectionsSpeed.compareTimeOfRemove(index, linkedList, "LinkedList");
 
-//        Files.delete(Path.of(PATH));
+        Files.delete(Path.of(PATH_TO_INPUT_FILE));
     }
 }
 
@@ -71,7 +70,7 @@ class TestCollectionsSpeed {
 
         System.out.println(comment);
         long diff = endTime - startTime;
-        System.out.println("> Время исполнения: " + diff);
+        System.out.println(String.format("> Время исполнения: %s мс", diff));
         System.out.println("Проверка индекса: " + list.get(index));
 
         return diff;
