e552e05929db7e07932ee4175d222e3ea9d0fa2d
Денис Кагане <kagane@yandex.ru>
2024-01-21 16:28:35 +0300

    сделал безопасно по рекомендации Sonar

--- src/main/java/regex/SelfClosingTag.java
+++ src/main/java/regex/SelfClosingTag.java
@@ -1,5 +1,11 @@
 package regex;
 
+import lombok.AllArgsConstructor;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SelfClosingTag {
     public static void main(String[] args) {
         String xml = """
@@ -14,13 +20,35 @@ public class SelfClosingTag {
     }
 
     public static String removeXmlStringNamespaceAndPreamble(String xmlString) {
-        return xmlString
-                .replaceAll("(<\\?[^<]*\\?>)?", "")/* remove preamble */
-                .replaceAll("\s+[\\w]+:(?!:)+xmlns.*?(\"|\').*?(\"|\')", "") /* remove xmlns declaration */
-//                .replaceAll("[\\w]+:(?!:)", "")/* remove prefix */
-                .replaceAll("\s+xmlns.*?(\"|\').*?(\"|\')", "")  /* remove xmlns declaration without prefix*/
-                .replaceAll("(<)(\\w+:)(.*?>)", "$1$3") /* remove opening tag prefix */
-                .replaceAll("(</)(\\w+:)(.*?>)", "$1$3") /* remove closing tags prefix */
-                .replaceAll("<(\\w+).*?></(\\w+).*?>","<$1/>");
+//        return xmlString
+//                .replaceAll("(<\\?[^<]*\\?>)?", "")/* remove preamble */
+//                .replaceAll("\s+[\\w]+:(?!:)+xmlns.*?(\"|\').*?(\"|\')", "") /* remove xmlns declaration */
+////                .replaceAll("[\\w]+:(?!:)", "")/* remove prefix */
+//                .replaceAll("\s+xmlns.*?(\"|\').*?(\"|\')", "")  /* remove xmlns declaration without prefix*/
+//                .replaceAll("(<)(\\w+:)(.*?>)", "$1$3") /* remove opening tag prefix */
+//                .replaceAll("(</)(\\w+:)(.*?>)", "$1$3") /* remove closing tags prefix */
+//                .replaceAll("<(\\w+).*?></(\\w+).*?>","<$1/>");
+
+        List<XMLPatterns> patterns = new ArrayList<>();
+        patterns.add(new XMLPatterns("(<\\?[^<]*\\?>)?", ""));
+        patterns.add(new XMLPatterns("\s+[\\w]+:(?!:)+xmlns.*?(\"|\').*?(\"|\')", ""));
+        patterns.add(new XMLPatterns("\s+xmlns.*?(\"|\').*?(\"|\')", ""));
+        patterns.add(new XMLPatterns("(<)(\\w+:)(.*?>)", "$1$3"));
+        patterns.add(new XMLPatterns("(</)(\\w+:)(.*?>)", "$1$3"));
+        patterns.add(new XMLPatterns("<(\\w+).*?></(\\w+).*?>", "<$1/>"));
+
+        for (XMLPatterns pattern : patterns) {
+            xmlString = Pattern.compile(pattern.regEx)
+                    .matcher(xmlString)
+                    .replaceAll(pattern.replacement);
+        }
+
+        return xmlString;
+    }
+
+    @AllArgsConstructor
+    private static class XMLPatterns {
+        String regEx;
+        String replacement;
     }
 }
