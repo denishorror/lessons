452782b48066967a91b0e0dc957a75b75379633d
Денис Кагане <kagane@yandex.ru>
2024-02-19 14:59:04 +0300

    сделал безопасно по рекомендации Sonar

--- src/main/java/regex/SelfClosingTag.java
+++ src/main/java/regex/SelfClosingTag.java
@@ -2,18 +2,18 @@ package regex;
 
 import lombok.AllArgsConstructor;
 
-import java.util.*;
-import java.util.regex.Matcher;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class SelfClosingTag {
     public static void main(String[] args) {
         String xml = """
-                <ns:tag xmlns="aaa bbb ccc"></tag>"
-                "<tag ns:xmlns = "всякая хрень">123</ns3:tag>"
-                "<ns22:PSK xmlns:ns22="http://www.rshb.ru/cdm/cft/get_person_liabilities/201711/resp">13.477</ns22:PSK>"
-                "<RepaymentScheme>Аннуитетные</RepaymentScheme>"
-                "<ns22:PDNScheduledRepaymentDate xmlns:ns22="http://www.rshb.ru/cdm/cft/get_person_liabilities/201711/resp">2024-06-25</ns22:PDNScheduledRepaymentDate>
+                <ns:tag xmlns="aaa bbb ccc"></tag>
+                <tag ns:xmlns = "всякая хрень">123</ns3:tag>
+                <ns22:PSK xmlns:ns22="http://www.rshb.ru/cdm/cft/get_person_liabilities/201711/resp">13.477</ns22:PSK>
+                <RepaymentScheme>Аннуитетные</RepaymentScheme>
+                <ns22:PDNScheduledRepaymentDate xmlns:ns22="http://www.rshb.ru/cdm/cft/get_person_liabilities/201711/resp">2024-06-25</ns22:PDNScheduledRepaymentDate>
                 """;
         System.out.println("xml:");
         System.out.println(removeXmlStringNamespaceAndPreamble(xml)); //
